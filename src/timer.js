793dea4171bad76842b4c8f5108dcf4fb0a0f556
Nick <nick.mueller@uni-greifswald.de>
2020-07-31 13:01:25 +0200

    Update

---
+++ src/timer.js
@@ -0,0 +1,15 @@
+class Timer {
+
+    constructor(minutes, seconds) {
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    print() {
+        return `Es sind noch ${this.minutes} Minuten und ${this.seconds} Sekunden`;
+    }
+
+    start() {
+        // TODO: start timer
+    }
+}
